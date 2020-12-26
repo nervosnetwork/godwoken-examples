9d1ac61624ed1330e98353787a6ef4ee061e0d2a
Linfeng Qian <thewawar@gmail.com>
2020-12-26 22:24:34 +0800

    Update Polyjuice API

--- packages/polyjuice/lib/index.js
+++ packages/polyjuice/lib/index.js
@@ -1,5 +1,5 @@
 
-const { UInt32LEToNumber, numberToUInt32LE } = require("@godwoken-examples/godwoken");
+const { UInt32LEToNumber, numberToUInt32LE, GodwokenUtils } = require("@godwoken-examples/godwoken");
 
 function encodeArgs(to_id, value, data) {
   const call_kind = to_id > 0 ? 1 : 3;
@@ -44,31 +44,11 @@ class Polyjuice {
   }
 
   async _send(method, from_id, to_id, value, data, nonce, signature) {
-    const args = encodeArgs(to_id, value, data);
-    const real_to_id = to_id > 0 ? to_id : this.creator_account_id;
-    const raw = {
-      from_id: numberToUInt32(from_id),
-      to_id: numberToUInt32(real_to_id),
-      nonce: numberToUInt32(nonce),
-      args,
-    };
     const l2tx = { raw, signature };
     const run_result = await method(l2tx);
     return run_result.return_data;
   }
 
-  async ethCall(from_id, to_id, value, data, nonce, signature) {
-    return await this._send(
-      this.client.executeL2Transaction,
-      from_id, to_id, value, data, nonce, signature,
-    );
-  }
-  async sendTransaction(from_id, to_id, value, data, nonce, signature) {
-    return await this._send(
-      this.client.submitL2Transaction,
-      from_id, to_id, value, data, nonce, signature,
-    );
-  }
   async getBalance(account_id) {
     return await this.client.getBalance(this.sudt_id, account_id);
   }
@@ -85,6 +65,16 @@ class Polyjuice {
   }
 
   // High level functions
+  generateTransaction(from_id, to_id, value, data, nonce, signature) {
+    const args = encodeArgs(to_id, value, data);
+    const real_to_id = to_id > 0 ? to_id : this.creator_account_id;
+    return {
+      from_id: numberToUInt32(from_id),
+      to_id: numberToUInt32(real_to_id),
+      nonce: numberToUInt32(nonce),
+      args,
+    };
+  }
   async generateCreateCreatorAccountTransaction(from_id, nonce) {
     const script_args_buf = Buffer.alloc(4);
     script_args_buf.writeUInt32LE(this.sudt_id);
@@ -93,7 +83,7 @@ class Polyjuice {
       hash_type: "data",
       args: `0x${script_args_buf.toString("hex")}`,
     };
-    return this.client.createAccountRawL2Transaction(from_id, nonce, script);
+    return GodwokenUtils.createAccountRawL2Transaction(from_id, nonce, script);
   }
 }
 
