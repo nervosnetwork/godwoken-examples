c5d166e74cfa623eb6558e1cd6bf24a8438a66c6
hupeng <bitrocks.hu@gmail.com>
2020-12-30 10:47:42 +0800

    feat: add withdrawal request api

--- packages/godwoken/index.d.ts
+++ packages/godwoken/index.d.ts
@@ -1,4 +1,3 @@
-
 import { Map } from "immutable";
 
 export type Uint32 = number;
@@ -15,52 +14,52 @@ export function numberToUInt32LE(value: number): HexString;
 export function UInt32LEToNumber(hex: HexString): number;
 export function hexToU32(hex: HexString): number;
 export function u32ToHex(value: number): HexString;
-export function toBuffer(ab: ArrayBuffer): Buffer ;
+export function toBuffer(ab: ArrayBuffer): Buffer;
 
 export interface RunResult {
-    read_values: Map<Hash, Hash>;
-    write_values: Map<Hash, Hash>;
-    return_data: HexString;
-    account_count?: HexNumber;
-    new_scripts: Map<Hash, HexString>;
-    write_data: Map<Hash, HexString>;
-    read_data: Map<Hash, HexNumber>;
+  read_values: Map<Hash, Hash>;
+  write_values: Map<Hash, Hash>;
+  return_data: HexString;
+  account_count?: HexNumber;
+  new_scripts: Map<Hash, HexString>;
+  write_data: Map<Hash, HexString>;
+  read_data: Map<Hash, HexNumber>;
 }
 export interface RawL2Transaction {
-    from_id: HexNumber;
-    to_id: HexNumber;
-    nonce: HexNumber;
-    args: HexString;
+  from_id: HexNumber;
+  to_id: HexNumber;
+  nonce: HexNumber;
+  args: HexString;
 }
 export interface L2Transaction {
-    raw: RawL2Transaction;
-    signature: HexString;
+  raw: RawL2Transaction;
+  signature: HexString;
 }
 
 export interface CreateAccount {
-    script: Script;
+  script: Script;
 }
 
 export interface RawWithdrawalRequest {
-    nonce: Uint32,
-    // CKB amount
-    capacity: Uint64,
-    // SUDT amount
-    amount: Uint128,
-    sudt_script_hash: Hash,
-    // layer2 account_script_hash
-    account_script_hash: Hash,
-    // buyer can pay sell_amount and sell_capacity to unlock
-    sell_amount: Uint128,
-    sell_capacity: Uint64,
-    // layer1 lock to withdraw after challenge period
-    owner_lock_hash: Hash,
-    // layer1 lock to receive the payment, must exists on the chain
-    payment_lock_hash: Hash,
+  nonce: HexNumber;
+  // CKB amount
+  capacity: HexNumber;
+  // SUDT amount
+  amount: HexNumber;
+  sudt_script_hash: Hash;
+  // layer2 account_script_hash
+  account_script_hash: Hash;
+  // buyer can pay sell_amount and sell_capacity to unlock
+  sell_amount: HexNumber;
+  sell_capacity: HexNumber;
+  // layer1 lock to withdraw after challenge period
+  owner_lock_hash: Hash;
+  // layer1 lock to receive the payment, must exists on the chain
+  payment_lock_hash: Hash;
 }
 export interface WithdrawalRequest {
-    raw: RawWithdrawalRequest,
-    signature: HexString,
+  raw: RawWithdrawalRequest;
+  signature: HexString;
 }
 
 // export interface HeaderInfo {
@@ -70,34 +69,47 @@ export interface WithdrawalRequest {
 // FIXME: todo
 // export interface L2Block {}
 export enum Status {
-    Running = "running",
-    Halting = "halting",
+  Running = "running",
+  Halting = "halting",
 }
 
 export declare class Godwoken {
-    constructor(url: string);
+  constructor(url: string);
 
-    executeL2Transaction(l2tx: L2Transaction): Promise<RunResult>;
-    submitL2Transaction(l2tx: L2Transaction): Promise<RunResult>;
-    submitWithdrawalRequest(request: WithdrawalRequest): void;
-    getBalance(sudt_id: Uint32, account_id: Uint32): Promise<Uint128>;
-    getStorageAt(account_id: Uint32, key: Hash): Promise<Hash>;
-    getAccountIdByScriptHash(script_hash: Hash): Promise<Uint32>;
-    getNonce(account_id: Uint32): Promise<Uint32>;
-    getScript(script_hash: Hash): Promise<Script>;
-    getScriptHash(account_id: Uint32): Promise<Hash>;
-    getData(data_hash: Hash): Promise<HexString>;
-    // gw_getDataHash
-    hasDataHash(data_hash: Hash): Promise<boolean>;
+  executeL2Transaction(l2tx: L2Transaction): Promise<RunResult>;
+  submitL2Transaction(l2tx: L2Transaction): Promise<RunResult>;
+  submitWithdrawalRequest(request: WithdrawalRequest): void;
+  getBalance(sudt_id: Uint32, account_id: Uint32): Promise<Uint128>;
+  getStorageAt(account_id: Uint32, key: Hash): Promise<Hash>;
+  getAccountIdByScriptHash(script_hash: Hash): Promise<Uint32>;
+  getNonce(account_id: Uint32): Promise<Uint32>;
+  getScript(script_hash: Hash): Promise<Script>;
+  getScriptHash(account_id: Uint32): Promise<Hash>;
+  getData(data_hash: Hash): Promise<HexString>;
+  // gw_getDataHash
+  hasDataHash(data_hash: Hash): Promise<boolean>;
 }
 
 export declare class GodwokenUtils {
-    constructor(rollup_type_hash: Hash);
-    generateTransactionMessageToSign(raw_l2tx: RawL2Transaction): Hash;
-    static generateWithdrawalMessageToSign(raw_request: RawWithdrawalRequest): Hash;
-    static createAccountRawL2Transaction(
-        from_id: Uint32,
-        nonce: Uint32,
-        script: Script,
-    ): RawL2Transaction;
+  constructor(rollup_type_hash: Hash);
+  generateTransactionMessageToSign(raw_l2tx: RawL2Transaction): Hash;
+  static generateWithdrawalMessageToSign(
+    raw_request: RawWithdrawalRequest
+  ): Hash;
+  static createAccountRawL2Transaction(
+    from_id: Uint32,
+    nonce: Uint32,
+    script: Script
+  ): RawL2Transaction;
+  static createRawWithdrawalRequest(
+    nonce: Uint32,
+    capacity: Uint64,
+    amount: Uint128,
+    sudt_script_hash: Hash,
+    account_script_hash: Hash,
+    sell_amount: Uint128,
+    sell_capacity: Uint64,
+    owner_lock_hash: Hash,
+    payment_lock_hash: Hash
+  ): RawWithdrawalRequest;
 }
