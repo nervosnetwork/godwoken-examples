e13e9525356653a52530b5baac0e7af1492cd13b
classicalliu <classicalliu@gmail.com>
2021-05-19 13:42:03 +0800

    Using flag in get-balance

--- packages/tools/src/account/get-balance.ts
+++ packages/tools/src/account/get-balance.ts
@@ -1,13 +1,9 @@
 import { Godwoken } from "@godwoken-examples/godwoken";
 import { Command } from "commander";
 
-export async function getBalance(
-  accountId: number,
-  sudtId = 1,
-  program: Command
-) {
-  accountId = +accountId;
-  sudtId = +sudtId;
+export async function getBalance(program: Command) {
+  const accountId = +program.accountId;
+  const sudtId = +program.sudtId;
 
   const godwoken = new Godwoken(
     program.parent.godwokenRpc,
